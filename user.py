daaaafd2b7656e608bf0c6372349aa080fef03e1
Bhanu <cherukuribhavana9@gmail.com>
2023-12-20 16:42:49 +0530

    positive

---
+++ user.py
@@ -0,0 +1,7 @@
+a={'username':'Bhanu','password':'Bhanu@2003'}
+b=input('enter username')
+c=input('enter password')
+if b,c==a:
+    print('login Sucssfull')
+else:
+    print('usename and password ivalid')
